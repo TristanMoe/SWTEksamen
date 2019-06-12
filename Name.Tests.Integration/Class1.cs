c96baeea336da41f8431b5357b843a15f8b77d1a
TristanMoe <Trilleo@hotmail.com>
2019-06-12 16:20:27 +0200

    Added testcase for jenkins

--- Name.Tests.Integration/Class1.cs
+++ Name.Tests.Integration/Class1.cs
@@ -8,7 +8,13 @@ using NSubstitute;
 
 namespace Name.Tests.Integration
 {
+    [TestFixture]
     public class Class2
     {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
     }
 }
